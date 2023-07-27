725583bfac25740a8b48c390dcc6340507852b65
LeoRdgz <leonardorodriggss@gmail.com>
2023-07-27 16:11:37 -0500

    Creación Clase Review

---
+++ Proyecto_HotelABC/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ubiety.Dns.Core.Records;
+
+namespace Proyecto_HotelABC.Entities
+{
+    public class Review
+    {
+        public 
+    }
+}
